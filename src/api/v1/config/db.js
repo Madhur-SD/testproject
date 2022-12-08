2b9f2707d2927b953ecbb3f7ef471de645018194
madhur999 <madhurgupta@fabloplatforms.com>
2022-12-08 20:08:42 +0530

    add database

--- src/api/v1/config/db.js
+++ src/api/v1/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost/demo', (err) => {
+const db = mongoose.connect('mongodb+srv://12345:12345@cluster0.b9h43w1.mongodb.net/?retryWrites=true&w=majority', (err) => {
     if (!err) {
         console.log('Connected to db')
     } else {
